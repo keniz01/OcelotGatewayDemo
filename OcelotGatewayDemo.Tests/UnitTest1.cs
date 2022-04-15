3606fbaedacd3bf01bdea07c089c399e354c1edf
Kenneth Kiiza <keniz01@Kenneths-MacBook-Pro.local>
2022-04-15 14:44:58 +0100

    Ocelot gateway demo initial solution structure

---
+++ OcelotGatewayDemo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OcelotGatewayDemo.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
